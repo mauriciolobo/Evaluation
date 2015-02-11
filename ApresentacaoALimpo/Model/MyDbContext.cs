365281a1e79ff086c3c6c4b57eb8494c3f4a46c0
mauricio.lobo <mauricio.lobo@solutionsone.com>
2015-02-11 14:45:36 -0200

    Creating database and seeding student table.

---
+++ ApresentacaoALimpo/Model/MyDbContext.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+
+namespace ApresentacaoALimpo.Model
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(string currentDb)
+            : base(currentDb)
+        {
+            Database.SetInitializer(new DatabaseCreator());
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Asset> Assets { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+    }
+
+    public class Enrollment
+    {
+        public int Id { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime ExitDate { get; set; }
+        public Section Section { get; set; }        
+    }
+
+    public class Section
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Asset
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime? Checkin { get; set; }
+        public DateTime? Checkout { get; set; }
+        public Student WithStudent { get; set; }
+    }
+
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+
+    class DatabaseCreator : DropCreateDatabaseIfModelChanges<MyDbContext>
+    {
+        protected override void Seed(MyDbContext context)
+        {
+            SeedStudents(context.Students);
+            base.Seed(context);
+        }
+
+        private void SeedStudents(DbSet<Student> students)
+        {
+            students.Add(new Student { FirstName = "Mauricio", LastName = "Lobo", Email = "mau@lobo.com" });
+            students.Add(new Student { FirstName = "Mayara", LastName = "Sa", Email = "mayara@sa.com" });
+        }
+    }
+}
---
+++ ApresentacaoALimpo/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ApresentacaoALimpo.Model;
+
+namespace ApresentacaoALimpo
+{
+    class Program
+    {
+        private static string embeddedSql = "mylocaldb";
+        private static string sqlServer = "mysqldb";
+
+        static void Main(string[] args)
+        {
+            var currentDb = sqlServer;
+            CreateDatabase(currentDb);
+
+            ExecuteExamQuestion(currentDb);
+
+            Console.WriteLine();
+            Console.WriteLine("Application finished! Press <any key> to quit.");
+            Console.ReadKey();
+        }
+
+        private static void ExecuteExamQuestion(string currentDb)
+        {
+            //Send email for all students who have any asset and fisished all courses with more than 10 days
+            var conn = new SqlConnection(ConfigurationManager.AppSettings[currentDb]);
+        }
+
+        #region Helper Methods
+        private static void CreateDatabase(string currentDb)
+        {
+            Console.WriteLine("Creating database...");
+            var db = new MyDbContext(currentDb);
+            db.Database.CreateIfNotExists();
+
+            ShowTableData(db.Students.ToList());
+            ShowTableData(db.Assets.ToList());
+            ShowTableData(db.Enrollments.ToList());
+        }
+
+        private static void ShowTableData<T>(List<T> list)
+        {
+            var t = typeof (T);
+            var tProperties = t.GetProperties();
+            Console.WriteLine();
+            Console.WriteLine("------- Table: " + t.Name + " -------");
+            Console.WriteLine(string.Join(";", tProperties.Select(p => p.Name)));
+            list.ForEach(l =>Console.WriteLine(string.Join(";", t.GetProperties().Select(p => p.GetValue(l, null)))));
+            Console.WriteLine(new string('-', 40));
+        }
+        #endregion
+    }
+}
